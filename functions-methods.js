257822eca7a6eeacec9c421d390e69ccf811bb98
nynkek <nyn.jaa@gmail.com>
2022-03-30 12:11:06 +0200

    opdracht functions en methodes is af

--- functions-methods.js
+++ functions-methods.js
@@ -1,4 +1,3 @@
-
 // Je gaat functies schrijven die we kunnen hergebruiken om sommige e-mailadressen te checken.
 // Nu zul je gaan merken hoe handig functies kunnen zijn!
 // Je zult hier methoden van het String Object voor nodig hebben, dus pak de paragraaf op EdHub over het String Object er even bij.
@@ -16,8 +15,8 @@ function getEmailDomain(emailadress) {
     return domainName;
 }
 
-const outcome = getEmailDomain("n.eeken@novi-education.nl");
-console.log(outcome)
+const outcome1 = getEmailDomain("n.eeken@novi-education.nl");
+console.log(outcome1)
 
 // of gewoon zo:
 console.log(getEmailDomain("t.mellink@novi.nl"));
@@ -28,10 +27,33 @@ console.log(getEmailDomain("a.wiersma@outlook.com"));
 // ---- Verwachte uitkomsten:
 // typeOfEmail("n.eeken@novi-education.nl") geeft "Student"
 // typeOfEmail("t.mellink@novi.nl") geeft geeft "Medewerker"
-// typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
+// typeOfEmail("novi.nlaapjesk@outlook.com") geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+function typeOfEmail(emailaddress) {
+    if (getEmailDomain(emailaddress) === "novi-education.nl") {
+        const outcome = "Student";
+        return outcome;
+    } else if (getEmailDomain(emailaddress) === "novi.nl") {
+        const outcome = "Medewerker";
+        return outcome;
+    } else {
+        const outcome = "Extern";
+        return outcome;
+    }
+}
 
+
+let outcome2 = typeOfEmail("n.eeken@novi-education.nl");
+console.log(outcome2)
+outcome2 = typeOfEmail("t.mellink@novi.nl");
+console.log(outcome2)
+outcome2 = typeOfEmail("novi.nlaapjesk@outlook.com");
+console.log(outcome2)
+outcome2 = typeOfEmail("a.wiersma@outlook.com");
+console.log(outcome2)
+outcome2 = typeOfEmail("a.wiersma@gmail.com");
+console.log(outcome2)
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -40,8 +62,23 @@ console.log(getEmailDomain("a.wiersma@outlook.com"));
 // * Er géén , in voorkomt
 // * Er géén . in voorkomt als allerlaatste karakter (dus hotmail.com is valide, net als outlook.nl, maar outlooknl. niet)
 // ---- Verwachte uitkomsten:
-// checkEmailValidity("n.eeken@novi.nl") geeft true - want @ en punt op de juiste plek
-// checkEmailValidity("tessmellink@novi.nl") geeft true - want @ en punt op de juiste plek
-// checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
-// checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
-// checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+function checkEmailValidity(emailaddress) {
+    if (emailaddress.includes("@") && !emailaddress[emailaddress.length - 1].includes(".") && !emailaddress.includes(",")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+let outcome3 = checkEmailValidity("n.eeken@novi.nl") //geeft true - want @ en punt op de juiste plek
+console.log(outcome3);
+outcome3 = checkEmailValidity("tessmellink@novi.nl")// geeft true - want @ en punt op de juiste plek
+console.log(outcome3);
+outcome3 = checkEmailValidity("n.eekenanovi.nl") //geeft false - want geen @
+console.log(outcome3);
+outcome3 = checkEmailValidity("n.eeken@novinl.") //geeft false - want de punt mag niet als laatst
+console.log(outcome3);
+outcome3 = checkEmailValidity("tessmellink@novi,nl") //geeft false - want er staat een komma in
+console.log(outcome3);
